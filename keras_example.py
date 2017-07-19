046e9805d31833813a579e8a92fb7c848947a48f
Walter Silva Martins Filho <walter@on.br>
2017-07-19 01:13:01 -0300

    update keras example

--- keras_example.py
+++ keras_example.py
@@ -1,6 +1,16 @@
 '''
 This is an example to how to work with Keras package. 
-You can find more in https://keras.io/#you-have-just-found-keras
+You can find more in 
+
+- https://keras.io/#you-have-just-found-keras
+
+- https://keras.io/getting-started/sequential-model-guide/
+
+What is a sequential model? 
+- https://en.wikipedia.org/wiki/Sequential_model
+- http://web.engr.oregonstate.edu/~tgd/publications/mlsd-ssspr.pdf
+- https://www.d.umn.edu/~rmaclin/cs8751/spring2003/talks/shar0201.pdf
+
 '''
 
 from keras.models import Sequential #a linear stack of layers
